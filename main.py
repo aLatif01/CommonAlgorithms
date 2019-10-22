7c42b7382f9de075ba743b329cf0050ddf14f644
Afnan Latif <afnan.latif01@gmail.com>
2019-10-22 11:18:52 -0500

    Starting of basic algorithms

---
+++ main.py
@@ -0,0 +1,14 @@
+#def: cross(size):
+#     for i in range(size+1)
+#         hash = "#"
+#         for j in range(size+1)
+#             if(i==j || i==j-1)
+#                 print(hash)
+#             else
+#                 print(" ")
+
+
+def: upsideDownTriangle(size):
+    for i in range(size+1)
+        star = "*"
+        for j in range(size+1)
